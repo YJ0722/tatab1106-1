44dde28e8b50ddd77927a4b26be70178f28706d7
soo <mss1994@naver.com>
2018-11-22 13:13:52 +0900

    board 에서 task 두줄

--- src/main/webapp/resources/js/topMenu/topMenu_projects.js
+++ src/main/webapp/resources/js/topMenu/topMenu_projects.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	$(document).on("mouseover", '#list', function(e) {
-        $(this).css('background-color','red');
+        $(this).css('background-color','darkorange');
     });
 	$(document).on("mouseout", '#list', function(e) {
 		$(this).css('background-color','darkgrey');
